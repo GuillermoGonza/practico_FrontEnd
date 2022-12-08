17c0c953efc90ba93a84067de8af67189a8e1406
GuillermoGonza <guillermoandresgonzalez79@gmail.com>
2022-12-08 19:47:24 -0300

    Breadcrumb
    Se corrigió y actualizo la parte de  Breadcrumb, titulo / icono de la ventana - se agrego argumentos a las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { PreciosComponent } from './pages/precios/precios.component';
+// import { PreciosComponent } from './pages/precios/precios.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ProductosComponent } from './pages/productos/productos.component';
 import { ContactoComponent } from './pages/contacto/contacto.component';
 import { ProductoComponent } from './pages/producto/producto.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'productos', component: ProductosComponent },
-  { path: 'producto/:ean', component: ProductoComponent },
-  { path: 'contacto', component: ContactoComponent },
-  { path: 'precios', component: PreciosComponent},
+  { path: 'home', component: HomeComponent, data: { titulo: 'Home'} },
+  { path: 'productos', component: ProductosComponent, data: { titulo: 'Productos'} },
+  { path: 'producto/:ean', component: ProductoComponent, data: { titulo: 'Producto'} },
+  { path: 'contacto', component: ContactoComponent, data: { titulo: 'Contacto'} },
+  // { path: 'precios', component: PreciosComponent},
   { path: '**', pathMatch: 'full', redirectTo: 'home'}
 
 ];
