4180ce957b1195a793350b1694d58436637fe4b6
Carlos Daniel selva <cardansel@gmail.com>
2022-12-08 21:18:33 -0300

    Add page Error404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,19 +6,20 @@ import { HomeComponent } from './pages/home/home.component';
 import { ProductosComponent } from './pages/productos/productos.component';
 import { ContactoComponent } from './pages/contacto/contacto.component';
 import { ProductoComponent } from './pages/producto/producto.component';
+import { Error404Component } from './pages/error404/error404.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'productos', component: ProductosComponent },
   { path: 'producto/:ean', component: ProductoComponent },
   { path: 'contacto', component: ContactoComponent },
-  { path: 'precios', component: PreciosComponent},
-  { path: '**', pathMatch: 'full', redirectTo: 'home'}
-
+  { path: 'precios', component: PreciosComponent },
+  { path: '**', component: Error404Component },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/shared/breadcrumb/breadcrumb.component.ts
+++ src/app/shared/breadcrumb/breadcrumb.component.ts
@@ -11,5 +11,7 @@ export class BreadcrumbComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+goBack() {
+    window.history.back();
+  }
 }
